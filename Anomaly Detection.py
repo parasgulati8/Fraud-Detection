7176b6e4eb68dfe816392e7ec680df8d50ca851f
Paras Gulati <parasgulati8@gmail.com>
2019-02-06 12:43:31 -0500

    Update Anomaly Detection.py

--- Anomaly Detection.py
+++ Anomaly Detection.py
@@ -33,11 +33,9 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(
         negative_x, negative_y, test_size=0.2, random_state=42)
 
+#Adding the positive examples to test set
 X_test.extend(positive_x)
 y_test.extend(positive_y)
-
-random.shuffle(X_test)
-random.shuffle(y_test)
 
 #Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -56,41 +54,27 @@ for j in range(X_train.shape[1]):
   std_X_train.append(np.std(X_train[:, j], axis=0)) 
   
 #Calculating the probability of each exaple in test set
-import scipy.stats
+import math
 y_pred = []
+def norm(x, mean, std):
+    variance = float(std)**2
+    deno = (2*math.pi*variance)**.5
+    numer = math.exp(-(float(x)-float(mean))**2/(2*variance))
+    return num/denom
+  
 for i in range(len(X_test)):
-    p = 1
-    for j in range(len(Mean_X_train)):
-        p *= scipy.stats.norm(Mean_X_train[j], std_X_train[j]).pdf(X_test[i, j])
-    y_pred.append(p)
+     p = 1
+     for j in range(len(Mean_X_train)):
+         p *= normpdf(X_test[i, j], Mean_X_train[j], std_X_train[j])
+     y_pred.append(p)
 
-#Calculating the probability of each exaple in training set
-import scipy.stats
-y_train_pred = []
-for i in range(len(X_train)):
-    p = 1
-    for j in range(len(Mean_X_train)):
-        p *= scipy.stats.norm(Mean_X_train[j], std_X_train[j]).pdf(X_train[i, j])
-    y_train_pred.append(p)
-
-#probabilities of all positive examples  
-indices_of_positives =[]
-for i in range(len(y_test)):
-    if y_test[i] == 1:
-        indices_of_positives.append(i)
-    
-probs_of_positives = []    
-for i in indices_of_positives:
-    probs_of_positives.append(y_pred[i])
-
-#Defining y_pred for a value of epsilon
+y_out = []
 for i in range(len(y_pred)):
-    if y_pred[i] < 3.914460408105776e-14 :
-        y_pred[i] = 1
+    if y_pred[i] < 9.914460408105776e-21 :
+        y_out.append(1) 
     else:
-        y_pred[i] = 0
-
+        y_out.append(0)
+        
 #checking efficiency with confusion matrix
 from sklearn.metrics import confusion_matrix
-confusion_matrix(y_test, y_pred)
-        
+confusion_matrix(y_test, y_out)
